01b17aaeb7c1f54edfeff0030abc5d53d0c01112
numbskul <matterider@gmail.com>
2024-11-24 18:37:45 -0800

    help2

---
+++ enemy.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var speed = 300
+var player_chase = false
+var player_shoot = false
+var player = null
+
+func _physics_process(delta: float) -> void:
+	
+	
+	rotate(get_angle_to(player.position()))
+	
+	var scene = preload("res://enemy projectile.tscn")
+	var instance = scene.instantiate()
+	if player_shoot:
+		instance.rotate(rotation)
+		instance.position = position
+		get_parent().add_child(instance)
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	player_shoot = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player = null
+	player_shoot = false
