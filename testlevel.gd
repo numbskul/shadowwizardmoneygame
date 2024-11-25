f59743351ac20e2dad2e30d213866133fd2bb3c8
numbskul <matterider@gmail.com>
2024-11-24 18:33:08 -0800

    asofn

--- testlevel.gd
+++ testlevel.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 	var scene = preload("res://node_2d.tscn")
 	var instance = scene.instantiate()
 	add_child(instance)
+	
+	var enemy = preload("res://enemy.tscn")
+	var inst = enemy.instantiate()
+	inst.position += Vector2.DOWN * 400
+	add_child(inst)
 	pass # Replace with function body.
 
 
