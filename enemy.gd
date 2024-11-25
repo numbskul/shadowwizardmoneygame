198f3d9e66ba7d80383f2862f7f9a35e82024284
numbskul <matterider@gmail.com>
2024-11-24 18:59:03 -0800

    please unbreak when i revert this

---
+++ enemy.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var speed = 300
+var player_chase = false
+var player_shoot = false
+var player = null
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	var scene = preload("res://enemy projectile.tscn")
+	var instance = scene.instantiate()
+	if player_shoot:
+		instance.rotate(rotation)
+		instance.position = position
+		get_parent().add_child(instance)
+		
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	player_shoot = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player = null
+	player_shoot = false
