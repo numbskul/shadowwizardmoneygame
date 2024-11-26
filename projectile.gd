a16740963d7c7391e2430a6db825818ffa478011
numbskul <matterider@gmail.com>
2024-11-25 22:29:33 -0800

    Changed proj movement system
    This should make collision detection easy, but it's late & i cannot figure out how to invert X & Y

--- projectile.gd
+++ projectile.gd
@@ -1,20 +1,18 @@
-extends Node2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	if test_move(transform, Vector2.UP.rotated(get_parent().rotation) * 20):
+		print("hit detected")
+	move_and_collide(Vector2.UP.rotated(get_parent().rotation) * 20)
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#check for collisions
-	if $RayCast2D.is_colliding():
-		# todo:
-		# 1. change rotation so it just changes xy movement rather than rotates 90 deg randomly
-		# 2. make collision behavior depend on object hit - destroy self & send signal on hit enemy or player
-		rotate(90)
-		$AudioStreamPlayer2D.play()
-	else:
-		position += Vector2.UP.rotated(rotation) * 15;
+
 	pass
