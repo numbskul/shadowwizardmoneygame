34558c2e9a9087c3af529fb74c24b0165c5f7040
numbskul <matterider@gmail.com>
2024-11-30 15:11:35 -0800

    noot
    game pauses on level start for 3 secs

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var is_paused = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,17 +9,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("pause_toggle"):
+		pause_toggle()
 	pass
 
-
+func pause_toggle():
+	is_paused = !is_paused;
+	get_tree().paused = is_paused
+	if is_paused:
+		show()
+	else:
+		hide()
+		
 func _on_quit_button_down():
 	pass # Replace with function body.
 
 
 func _on_main_menu_button_down():
+	get_tree().paused = false
+	SignalBus.restart.emit()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	pass # Replace with function body.
 
 
 func _on_resume_button_down():
+	pause_toggle()
 	pass # Replace with function body.
--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 var totalscore = 0
 var is_paused = false
+var gamestarted = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +9,17 @@ func _ready():
 	$PauseMenu.hide()
 	$Score.text = "Score: " + str(totalscore)
 	$DeathMsg.hide()
+	start_countdown()
 	pass # Replace with function body.
 
+func start_countdown():
+	$StartScreen/StartTimer.start()
+	get_tree().paused = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("pause_toggle"):
-		pause()
+	if !gamestarted:
+		$StartScreen/VBoxContainer/Countdown.text = str(ceil($StartScreen/StartTimer.time_left))
 	pass
 
 func _on_enemy_death(score):
@@ -37,10 +42,15 @@ func _on_restart_button_button_down():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	pass # Replace with function body.
 	
-func pause():
-	is_paused = !is_paused;
-	get_tree().paused = is_paused
-	if is_paused:
-		$PauseMenu.show()
-	else:
-		$PauseMenu.hide()
+
+
+func _on_start_timer_timeout():
+	$StartScreen/VBoxContainer/Label.text = "Go!"
+	get_tree().paused = false
+	var gamestarted = true
+	$Score.show()
+	$HP.show()
+	$StartScreen/VBoxContainer/Countdown.hide()
+	await get_tree().create_timer(1).timeout
+	$StartScreen.hide()
+	pass # Replace with function body.
