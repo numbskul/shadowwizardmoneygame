8b499b404189abd1b15a3f05d4d403aa2d37d9ef
numbskul <matterider@gmail.com>
2024-11-29 22:41:38 -0800

    pause menu
    we chillin (i hate my life)

---
+++ scenes/pause_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_quit_button_down():
+	pass # Replace with function body.
+
+
+func _on_main_menu_button_down():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	pass # Replace with function body.
+
+
+func _on_resume_button_down():
+	pass # Replace with function body.
--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 var totalscore = 0
+var is_paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalBus.enemy_death.connect(_on_enemy_death)
+	$PauseMenu.hide()
 	$Score.text = "Score: " + str(totalscore)
 	$DeathMsg.hide()
 	pass # Replace with function body.
@@ -11,6 +13,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("pause_toggle"):
+		pause()
 	pass
 
 func _on_enemy_death(score):
@@ -32,3 +36,11 @@ func _on_restart_button_button_down():
 	SignalBus.restart.emit()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	pass # Replace with function body.
+	
+func pause():
+	is_paused = !is_paused;
+	get_tree().paused = is_paused
+	if is_paused:
+		$PauseMenu.show()
+	else:
+		$PauseMenu.hide()
