cdfaaea3b15fce5f03ee53da27fbb542ea0db0ae
numbskul <matterider@gmail.com>
2024-11-30 20:05:36 -0800

    level select
    essentially done w menus now I think? really dunno what to do next, might fiddle w lighting

---
+++ scenes/level_select.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_lv_1_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_1.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_2_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_2.tscn")
+	pass # Replace with function body.
