78547264b0e5209634c1a46a21e21b4033c3a066
numbskul <matterider@gmail.com>
2024-12-01 18:33:24 -0800

    level select

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -24,3 +24,33 @@ func _on_lv_2_button_down():
 func _on_lv_4_button_down():
 	get_tree().change_scene_to_file("res://scenes/lv_4.tscn")
 	pass # Replace with function body.
+
+
+func _on_lv_3_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_3.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_5_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_5.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_6_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_6.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_7_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_7.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_8_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_8.tscn")
+	pass # Replace with function body.
+
+
+func _on_lv_9_button_down():
+	get_tree().change_scene_to_file("res://scenes/lv_9.tscn")
+	pass # Replace with function body.
