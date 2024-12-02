6cfe008fa02bf7139cb85535fd9f98b05fe731f1
Jadenlong <jadenlong376@gmail.com>
2024-12-01 18:24:37 -0800

    amongi

---
+++ scripts/testlevel.gd
@@ -0,0 +1,16 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_audio_stream_player_finished():
+	$AudioStreamPlayer.play()
+	pass # Replace with function body.
